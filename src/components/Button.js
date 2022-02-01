086fa23fdbb54b460a68ae9b2d8cb4930e285f20
Zaid Hasan Siddiqui <zaid.hasan.sddq@gmail.com>
2022-02-02 01:04:32 +0530

    init and final commit

---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
+class Button extends React.Component {
+  renderButton = (color) => {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(context) =>
+            context.language === "english" ? "Submit" : "Voorleggen"
+          }
+        </LanguageContext.Consumer>
+      </button>
+    );
+  };
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Context = React.createContext("english");
+
+export class LanguageStore extends React.Component {
+  state = { language: "english" };
+  onLanguageChange = (lang) => {
+    this.setState({ language: lang });
+  };
+  render() {
+    return (
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
